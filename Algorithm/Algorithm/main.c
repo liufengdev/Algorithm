3197ef0d5200b2d0e4c300e7e2cb42d252e3598f
liufengdev <510498872@qq.com>
2016-07-17 12:49:05 +0800

    最大子列和四种算法

---
+++ Algorithm/Algorithm/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Algorithm
+//
+//  Created by 刘锋 on 16/7/17.
+//  Copyright © 2016年 com.weidian. All rights reserved.
+//
+
+#include <stdio.h>
+#include "MaxSubSequenceSum.h"
+
+int main(int argc, const char * argv[]) {
+    
+    const int A[] = {4, -3, 5, -2, -1, 2, 6, -2};
+    int num = 8;
+    int ret = maxSubSequenceSum_N(A, num);
+    printf("-----------\n%d\n-----------\n", ret);
+    return 0;
+}
