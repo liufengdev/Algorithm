e61c99bde228aa67565eb7771d41f4e6f51979d2
liufengdev <510498872@qq.com>
2016-07-20 10:08:54 +0800

    链表声明

---
+++ Algorithm/Algorithm/LinkedList.c
@@ -0,0 +1,15 @@
+//
+//  LinkedList.c
+//  Algorithm
+//
+//  Created by 刘锋 on 16/7/20.
+//  Copyright © 2016年 com.weidian. All rights reserved.
+//
+
+#include "LinkedList.h"
+
+struct Node
+{
+    ElementType Element;
+    Position Next;
+};
